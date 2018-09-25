fbab106c332248192d00d1c84ab16bb90e4a8096
antonpalsson <antonpaalsson@gmail.com>
2018-09-25 17:14:57 +0200

    get pixel data

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Neural-Network-Character-Recognition
+
+Links we use:
+- https://js.tensorflow.org (Tensorflow)
+- https://js.tensorflow.org/tutorials/mnist.html
+
+- https://p5js.org (p5)
+
+- http://colah.github.io/posts/2014-07-Understanding-Convolutions/ (Understanding-Convolutions)
--- sketch.js
+++ sketch.js
@@ -1,15 +1,39 @@
 
+
 function setup() {
-    createCanvas(800, 800);
-    background(230);
-  }
-  
-  function draw() {
+  createCanvas(280, 280)
+  background(0)
+  stroke(255)
+  strokeWeight(3)
 
-  }
+  resetSketch()
 
-  function mouseDragged() 
-{ 
-	strokeWeight(8);
-	line(mouseX, mouseY, pmouseX, pmouseY);
+  var resetButton = createButton("reset")
+  resetButton.mousePressed(resetSketch)
+
+  var saveButton = createButton("save")
+  saveButton.mousePressed(saveSketch)
+}
+
+function resetSketch() {
+  console.log("resetSketch")
+  clear()
+  background(0)
+  stroke(255)
+  strokeWeight(3)
+}
+
+function saveSketch() {
+  console.log("saveSketch")
+
+  loadPixels()
+  console.log(pixels)
+}
+
+function mouseDragged() {
+  line(mouseX, mouseY, pmouseX, pmouseY);
+}
+
+function draw() {
+
 }
