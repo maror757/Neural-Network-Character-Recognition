218ff021fa6f8175b6e4e4cb0cc4e31012028ba2
maror757 <maror757@student.liu.se>
2018-10-15 14:05:20 +0200

    added whitespace

--- src/ui.js
+++ src/ui.js
@@ -5,6 +5,6 @@ document.body.appendChild(div);
 const statusElement = document.getElementById('status');
 
 export function log(message) {
-  div.innerText = 'Status text:'
+  div.innerText = 'Status text: \u00A0'
   statusElement.innerText += message;
 }
